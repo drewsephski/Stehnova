5c9e1d17c415750abe4d60150b751cf65d3e80e3
drewsephski <drewsepeczi@gmail.com>
2026-01-09 20:37:32 -0600

    formspree yadig

--- components/ui/combined-form-section.tsx
+++ components/ui/combined-form-section.tsx
@@ -35,7 +35,7 @@ export function CombinedFormSection() {
     formData.append('contactPath', contactPath)
     
     try {
-      const response = await fetch('https://formspree.io/f/meeoyogd', {
+      const response = await fetch('https://formspree.io/f/xnjneapa', {
         method: 'POST',
         body: formData,
         headers: {
